0e94e805242f2c7d28f03c6d900cbf5bf62a3201
John Anderson <info@soaringpenguin.com>
2025-04-19 22:43:29 -0700

    Start coding overall structure and header.

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-get_header();
-if ( have_posts() ) : while ( have_posts() ) : the_post();
-get_template_part( 'entry' );
-comments_template();
-endwhile; endif;
-get_template_part( 'nav', 'below' );
-get_footer();
+    get_header();
+    if ( have_posts() ) : while ( have_posts() ) : the_post();
+        get_template_part( 'entry' );
+    endwhile; endif;
+    // get_template_part( 'nav', 'below' );
+    get_footer();
---
+++ template-parts/header-content.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Header content
+ */
+?>
+<div class="header-bar">
+    <a href="/"><?php echo 'Header Bar'; ?></a>
+    <div class="header-bar__logo"><a href="/"><img src="<?php echo CORE_URL; ?>/assets/Rain-City-Comicon-logo-with-rain-animation-base.gif" /></a></div>
+</div>
