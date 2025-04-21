9effb2aa1497df2ef1cd62a6c496568f95807f6f
John Anderson <info@soaringpenguin.com>
2025-04-20 17:25:08 -0700

    Style header.

--- template-parts/header-content.php
+++ template-parts/header-content.php
@@ -3,7 +3,26 @@
  * Header content
  */
 ?>
-<div class="header-bar">
-    <a href="/"><?php echo 'Header Bar'; ?></a>
-    <div class="header-bar__logo"><a href="/"><img src="<?php echo CORE_URL; ?>/assets/Rain-City-Comicon-logo-with-rain-animation-base.gif" /></a></div>
-</div>
+    <?php if( is_home() || is_front_page() ): ?>
+        <div class="header-bar">
+            <?php // countdown ?>
+            <div class="countdown">
+                <div class="countdown__days">
+                    23<span>days</span>
+                </div>
+                <div class="countdown__hours">
+                    17<span>hours</span>
+                </div> 
+                <div class="countdown__minutes">
+                    45<span>minutes</span>
+                </div>
+                <div class="countdown__seconds">
+                    30<span>seconds</span>
+                </div>                                                           
+            </div>
+        </div>
+    <?php endif; ?>
+    <?php if( is_home() || is_front_page() ): ?>
+        <div class="header__logo"><img src="<?php echo CORE_URL; ?>/assets/Rain-City-Comicon-logo-with-rain-animation-base.gif" /></div>
+    <?php endif; ?>
+
