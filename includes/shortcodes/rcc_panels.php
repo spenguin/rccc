807ccdb1f4fa721a57ee7a27b2bde98fd8732946
John Anderson <info@soaringpenguin.com>
2025-05-12 11:53:43 -0700

    Style panels.

--- includes/shortcodes/rcc_panels.php
+++ includes/shortcodes/rcc_panels.php
@@ -7,7 +7,10 @@ function rcc_panels( $atts = [], $content = null, $tag = '' )
 {
     $args = [
         'post_type'         => 'panel',
-        'posts_per_page'    => -1
+        'posts_per_page'    => -1,
+        'orderby'           => 'meta_value_num',
+        'meta_key'          => 'start_time',
+        'order'             => 'ASC'
     ];
 
     $query  = new WP_Query($args);
@@ -15,9 +18,17 @@ function rcc_panels( $atts = [], $content = null, $tag = '' )
     ob_start();
     if( $query->have_posts()):  ?>
         <section class="panels">
-            <?php while( $query->have_posts() ): $query->the_post(); ?>
+            <?php while( $query->have_posts() ): $query->the_post(); 
+                $custom = get_post_custom();
+
+            ?>
                 <div class="panel">
-                    <?php echo the_content(); ?>
+                    <div class="panel__time">
+                        <?php echo $custom['start_time'][0] . ' to ' . $custom['end_time'][0]; ?>
+                    </div>
+                    <div class="panel__description">
+                        <?php echo the_content(); ?>
+                    </div>
                 </div>
             <?php endwhile; ?>
         </section>
