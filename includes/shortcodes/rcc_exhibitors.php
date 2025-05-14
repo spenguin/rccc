97ce151b911dc053155c1ed63454e6affd2f569d
John Anderson <info@soaringpenguin.com>
2025-05-13 18:29:41 -0700

    Start to build out Exhibitors page.

--- includes/shortcodes/rcc_exhibitors.php
+++ includes/shortcodes/rcc_exhibitors.php
@@ -14,10 +14,34 @@ function rcc_exhibitors( $atts = [], $content = null, $tag = '' )
 
     ob_start();
     if( $query->have_posts()):  ?>
-        <section class="exhibitors  card--list">
-            <?php while( $query->have_posts() ): $query->the_post(); ?>
+        <section class="exhibitors card--list">
+            <?php while( $query->have_posts() ): $query->the_post(); 
+                $facebook_handle    = get_post_meta( $query->post->ID, 'facebook_handle', TRUE ); 
+                $instagram_handle   = get_post_meta( $query->post->ID, 'instagram_handle', TRUE ); 
+                $twitter_handle     = get_post_meta( $query->post->ID, 'twitter_handle', TRUE ); 
+                $tiktok_handle      = get_post_meta( $query->post->ID, 'tiktok_handle', TRUE );
+                $website            = get_post_meta( $query->post->ID, 'website', TRUE );            
+            
+            ?>
                 <div class="exhibitor card--list__item">
                     <h3><?php echo the_title(); ?></h3>
+                    <div class="card--list__item--social-media">
+                        <?php if(!empty($facebook_handle)): ?>
+                            <a href="https://facebook.com/<?php echo $facebook_handle; ?>" target="_blank"><i class="fa-brands fa-square-facebook"></i></a>
+                        <?php endif; ?>
+                        <?php if(!empty($instagram_handle)): ?>
+                            <a href="https://instagram.com/<?php echo $instagram_handle; ?>" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
+                        <?php endif; ?>   
+                        <?php if(!empty($twitter_handle)): ?>
+                            <a href="https://x.com/<?php echo $twitter_handle; ?>" target="_blank"><i class="fa-brands fa-square-twitter"></i></a>
+                        <?php endif; ?>                                       
+                        <?php if(!empty($tiktok_handle)): ?>
+                            <a href="https://tiktok.com/<?php echo $tiktok_handle; ?>" target="_blank"><i class="fa-brands fa-tiktok"></i></a>
+                        <?php endif; ?>                                      
+                        <?php if(!empty($website)): ?>
+                            <a href="<?php echo $website; ?>" target="_blank"><img src="<?php echo CORE_URL . '/assets/icons/icons8-website-50.png'; ?>"></a>
+                        <?php endif; ?>                                    
+                    </div>                    
                 </div>
             <?php endwhile; ?>
         </section>
